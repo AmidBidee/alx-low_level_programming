36a99de84419171088bbf225fba1137d15148866
krummitz <amidbidee@gmail.com>
2021-06-17 19:27:56 +0100

    '-'

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: returns 0
+ */
 int main(void)
 {
-	printf("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
