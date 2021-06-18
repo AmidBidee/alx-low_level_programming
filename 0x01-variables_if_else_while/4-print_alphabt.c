e72d58d4e826124a8679fad58bbb01c31d1bd11e
krummitz <amidbidee@gmail.com>
2021-06-18 18:55:10 +0100

    print numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a_z;
+
+	for (a_z = 'a' ; a_z <= 'd' ;  ++a_z)
+	{
+		putchar(a_z);
+	}
+	putchar('\n');
+	return (0);
+
+}
+
