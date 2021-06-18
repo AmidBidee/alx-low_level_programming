32efed8dd0be655c3d34976fa074272b82968699
krummitz <amidbidee@gmail.com>
2021-06-18 21:34:56 +0100

    '-'

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,9 @@ int main(void)
 
 	for (n = 0 ; n < 10 ; ++n)
 	{
-		if (n == 9)
+		putchar((n % 10) + '0');
+		if (n != 9)
 		{
-			putchar((n % 10) + '0');
-		}
-		else
-		{
-			putchar((n % 10) + '0');
 			putchar(',');
 			putchar(' ');
 		}
