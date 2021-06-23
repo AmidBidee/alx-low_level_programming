c0247d96dc401c3bb56e01d5b9618a472c6167d7
krummitz <amidbidee@gmail.com>
2021-06-23 16:05:28 +0100

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+
+	int c;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
