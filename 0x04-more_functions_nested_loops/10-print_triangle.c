c4d130a2258b6bd15239c3428f34067d064bfdb8
krummitz <amidbidee@gmail.com>
2021-06-25 19:29:15 +0100

    fixed triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
- * print_diagonal - Entry point
- * @n: param
+ * print_triangle - Entry point
+ * @size: param
  * Description: length of diagonal line
  */
 void print_triangle(int size)
 {
 	int c, i = 0;
-	
+
 	int space;
-	
+
 	if (size > 0)
 	{
 		while (i < size)
