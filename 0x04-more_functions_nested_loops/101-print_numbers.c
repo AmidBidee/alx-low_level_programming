c1b4f9bc8cdf0f8fa2bc97e1222b403bcd0f72af
krummitz <amidbidee@gmail.com>
2021-06-25 21:14:57 +0100

    '-'

---
+++ 0x04-more_functions_nested_loops/101-print_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_number - Entry point
+ * @n: integer nau
+ */
+void print_number(int n)
+{
+	unsigned int n_1;
+
+	if(n < 0)
+	{
+		n_1 = -n;
+		_putchar('-');
+	}
+	else
+	{
+		n_1 = n;
+	}
+	if (n_1 / 10)
+	{
+		print_number(n_1 / 10);
+	}
+	_putchar((n_1 % 10) + '0');
+}
