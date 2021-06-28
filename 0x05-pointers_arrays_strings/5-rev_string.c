40303c72f98666afb9aebc5ab64eeba69252e361
krummitz <amidbidee@gmail.com>
2021-06-28 18:14:56 +0100

    task 4 and 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * _strlen - Return length of string
  * @s: param
@@ -10,9 +9,9 @@
 int _strlen(char *s)
 {
 	int i, string_len;
-	
+
 	char *string = s;  /* copy string stored from point */
-	
+
 	for (i = 0; string[i] != '\0'; i++)
 	{
 		if (string[i] != '\0')
@@ -21,11 +20,10 @@ int _strlen(char *s)
 			break;
 	}
 	string_len = i;
-	
+
 	return (string_len);
 	return (0);
 }
-
 /**
  * rev_string - updates and reverses a string
  * @s: param
@@ -33,25 +31,18 @@ int _strlen(char *s)
  */
 void rev_string(char *s)
 {
-	int i, r, len; /* i -> main index, r -> reversed index, len -> length of string */
+	int i, r, len; /* i -> index, r -> reversed, len -> length */
 
 	char *string = s;
 
-	len = _strlen(string);
+	char rev;
 
-	while (i < len)
+	len = _strlen(string);
+	i = len;
+	for (i--, r = 0; r < len / 2; i--, r++)
 	{
-		for (r = len; r >= 0; r--)
-		{
-			 if (i != 0)
-			 {
-				 *(s + i) = string[r];
-			 }
-			 else
-			 {
-				 *s = string[r];
-			 }
-		}
-		i++;
+		rev = s[r];
+		s[r] = s[i];
+		s[i] = rev;
 	}
 }
