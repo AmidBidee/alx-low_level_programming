b0064d49106e821618bf624700dadf14bf7a14ee
krummitz <amidbidee@gmail.com>
2021-07-01 16:40:48 +0100

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strcat - concatenate both strings
+ * @dest: params
+ * @src: parms
+ * Description: strings to be concatenated
+ * 
+ * Return: a pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	while (*dest++ != '\0')
+		;
+
+	while ((*dest++ = *src++))
+		;
+
+	return (dest);
+}
