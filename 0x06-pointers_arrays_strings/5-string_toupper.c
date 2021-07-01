5ed7449e0b546c6df3cd3fffeef69d53979fc58d
krummitz <amidbidee@gmail.com>
2021-07-01 22:17:07 +0100

    toupper task

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * string_toupper - Entry point
+ * @c: param
+ * Description: string
+ * Return: a pointer
+ */
+char *string_toupper(char *s)
+{
+	int i, j;
+
+	i = j = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+		i++;
+		j++;
+	}
+
+	return (s);
+}
