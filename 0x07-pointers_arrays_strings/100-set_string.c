2e2ea5d2b07897aef120c25c13cf397f449eba9c
krummitz <amidbidee@gmail.com>
2021-07-05 21:28:31 +0100

    tasks 7 and 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * set_string - sets the value of pointer to a char
+ * @s: string
+ * @to: pointer to be changed
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -9,4 +9,8 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 
 char *_strchr(char *s, char c);
 
+void set_string(char **s, char *to);
+
+void print_diagsums(int *a, int size);
+
 #endif
