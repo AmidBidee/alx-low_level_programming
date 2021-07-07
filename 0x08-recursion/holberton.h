22d89ff3d3696990ee7af51c52c774786a409289
krummitz <amidbidee@gmail.com>
2021-07-07 21:52:46 +0100

    tasks

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+
+int is_palindrome(char *s);
+
+int get_length(char *s);
+
+int my_pal(char *s, int l);
+
+void _print_rev_recursion(char *s);
+
+#endif
