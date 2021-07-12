22a7dc48c76b64988f384c19e0a990440ab14ff0
krummitz <amidbidee@gmail.com>
2021-07-12 19:37:51 +0100

    task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * main - prints the name of the program
+ * @argc: argument count in integer
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc-argc]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+
+int main(int argc, char *argv[]);
+
+int _putchar(char c);
+
+#endif
