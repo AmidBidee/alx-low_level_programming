a6ceb3b3dd80f1b22871ef24a531b6f30a963414
krummitz <amidbidee@gmail.com>
2021-07-12 19:55:00 +0100

    task 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * main - prints the number of args passed
+ * @argc: argument count in integer
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%i\n", argc);
+	return (0);
+}
