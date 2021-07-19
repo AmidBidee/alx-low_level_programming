aa85d0eb3e39851f06773bfb6cf4ebd6ea085c7b
krummitz <amidbidee@gmail.com>
2021-07-19 19:49:43 +0100

    todays tasks

---
+++ 0x0D-preprocessor/holberton.h
@@ -0,0 +1,22 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+char *_memset(char *s, char b, unsigned int n);
+
+int _putchar(char c);
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_strchr(char *s, char c);
+
+void set_string(char **s, char *to);
+
+void print_diagsums(int *a, int size);
+
+void print_chessboard(char (*a)[8]);
+
+char *_strpbrk(char *s, char *accept);
+
+char *_strstr(char *haystack, char *needle);
+
+#endif
