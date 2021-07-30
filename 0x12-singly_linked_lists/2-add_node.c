dafdea20ae8724d7b74bb7dcf622ffc40934b50a
krummitz <amidbidee@gmail.com>
2021-07-30 02:36:55 +0100

    fixes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -25,9 +25,9 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	new_node->len = _strlen(strdup(str));
-	new_node->next = (*head);
+	new_node->next = *head;
 
-	(*head) = new_node;
+	*head = new_node;
 
 	return (*head);
 }
