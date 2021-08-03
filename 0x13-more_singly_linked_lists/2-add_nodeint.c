d58dfde7a084e49252ab05a446cb50dc7ed1a072
krummitz <amidbidee@gmail.com>
2021-08-03 14:15:49 +0100

    fixes

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,13 +12,14 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	new_node->n = n;
 
-	if (*head == NULL)
-	{
-		*head = new_node;
-		new_node->next = NULL;
-		return (new_node);
-	}
-
+/*
+ *	if (*head == NULL)
+ *	{
+ *		*head = new_node;
+ *		new_node->next = NULL;
+ *		return (new_node);
+ *	}
+ */
 	if (new_node == NULL)
 	{
 		free(new_node);
