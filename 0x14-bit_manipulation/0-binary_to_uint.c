070c02b38ceca9b7e1772bfcacfb97539fa6db64
krummitz <amidbidee@gmail.com>
2021-08-06 22:39:57 +0100

    fixes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,9 +6,9 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-       	unsigned int num, power;
+	unsigned int num, power;
 	int str_len = _strlen(b);
-	int k;	
+	int k;
 
 	if (b == NULL)
 		return (0);
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 		if (b[k] != '0' && b[k] != '1')
 			return (0);
 	}
-	
+
 	for (power = 1, num = 0, str_len--; str_len >= 0; str_len--, power *= 2)
 	{
 		if (b[str_len] == '1')
