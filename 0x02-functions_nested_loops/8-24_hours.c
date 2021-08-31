e1b91cb06fc2593f9d8a441ed2bfb4891908144e
krummitz <amidbidee@gmail.com>
2021-08-31 01:02:06 +0100

    fixes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,7 +7,7 @@ void jack_bauer(void)
 	char time_arr[] = {'0', '0', '0', '0'}; /* hour 0 hour 0 min 0 minute 0 */
 	int min_count = 0, hr_count = 0;
 
-	while (hr_count != 24 && min_count != 61)
+	while (hr_count < 25 && min_count < 61)
 	{
 		if (time_arr[3] != '9')
 			++time_arr[3];
@@ -16,6 +16,7 @@ void jack_bauer(void)
 			time_arr[2]++;
 			time_arr[3] = '0';
 		}
+
 		if (min_count == 60)
 		{
 			time_arr[2] = '0';
@@ -35,12 +36,12 @@ void jack_bauer(void)
 				break;
 			min_count = 0;
 		}
-		min_count++;
-		_putchar(time_arr[0]);
+        _putchar(time_arr[0]);
 		_putchar(time_arr[1]);
 		_putchar(':');
 		_putchar(time_arr[2]);
 		_putchar(time_arr[3]);
 		_putchar('\n');
+        min_count++;
 	}
 }
